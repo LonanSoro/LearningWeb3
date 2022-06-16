0e9bd792f61e1932405d41ccd5b47eb41e510703
LONAN SORO <lonansoro@LONANs-MacBook-Pro.local>
2022-06-16 14:03:19 -0700

    First commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+
+module.exports = {
+  solidity: "0.8.4",
+
+  networks: {
+    rinkeby: {
+      // How do we define which test network to deploy to
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [PRIVATE_KEY],
+
+    },
+  },
+};
